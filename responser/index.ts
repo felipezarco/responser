df3e29a01ece2facbd201070ecfeeec17ba1a3c9
Felipe Zarco <felipezarco@hotmail.com>
2020-05-14 22:06:13 -0300

    add default responses for all status

--- responser/index.ts
+++ responser/index.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from 'express'
+import HttpStatus from 'http-status-codes'
+
+const camelCase = (str: string) => str.toLowerCase().replace(/(\_\w)/g, c => c[1].toUpperCase())
 
 const responser = (request: Request, response: Response, next: NextFunction) => {
-
-  response.ok = function() {
-    this.status(200).json({ message: 'Hello Responser' })
-  }
-
+  for(const [status, code] of Object.entries(HttpStatus)) {
+    if(status == 'getStatusText' || status == 'getStatusCode') continue
+    response[camelCase(status)] = function(message: string, content?: object) {
+      this.status(HttpStatus.OK).json({ 
+        status, 
+        code,
+        success: String(code).charAt(0) === '2',
+        message, 
+        data: { ...content }
+      })
+    }
+  }  
   next()
 } 
 
